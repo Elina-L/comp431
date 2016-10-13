56d6ef035186773b6b1115ff2230f2153e5b510e
Elina-L <liuelina@hotmail.com>
2016-10-13 17:23:47 -0500

    Update simple.js

--- inclass-15/simple.js
+++ inclass-15/simple.js
@@ -25,10 +25,52 @@ function server(req, res) {
      console.log('Request content-type  :', req.headers['content-type'])
      console.log('Request payload       :', req.body)
 
-     var payload = { 'hello': 'world' }
-     res.setHeader('Content-Type', 'application/json')
-     res.statusCode = 200
-     res.end(JSON.stringify(payload))
+     // var payload = { 'hello': 'world' }
+     // res.setHeader('Content-Type', 'application/json')
+     // res.statusCode = 200
+     // res.end(JSON.stringify(payload))
+
+
+     if (req.method == "GET") {
+          if (req.url == "/") {
+               var payload = { 'hello': 'world' }
+               res.setHeader('Content-Type', 'application/json')
+               res.statusCode = 200
+               res.end(JSON.stringify(payload))      
+          }
+          if (req.url == "/articles") {
+               var payload = { articles: 
+                    [ { id:1, author: 'Scott', body:'A post' },
+                      { id:2, author: 'Mike', body:'Another post' },
+                      { id:3, author: 'Greca', body:'Final post' }]};
+               res.setHeader('Content-Type', 'application/json')
+               res.statusCode = 200
+               res.end(JSON.stringify(payload))      
+          }
+          else {
+               // Don't have anything here...
+          }
+     }
+
+     if (req.method == "POST") {
+          if (req.url == "/login") {
+               var payload = req.body + " result: success}";
+               res.setHeader('Content-Type', 'application/json')
+               res.statusCode = 200
+               res.end(JSON.stringify(payload))      
+          }
+     }
+
+     if (req.method == "PUT") {
+          if (req.url == "/logout") {
+               var payload = "OK";
+               res.setHeader('Content-Type', 'application/json')
+               res.statusCode = 200
+               res.end(JSON.stringify(payload))      
+          }
+     }
+
+
 }
 
 // articles => /articles
